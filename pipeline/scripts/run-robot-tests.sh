cddc9ec4c7b083d8c065ece433d50af96357285f
ponchotitlan <asandovalros@gmail.com>
2025-05-04 12:58:50 +0100

    Initial commit

---
+++ pipeline/scripts/run-robot-tests.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -x
+# Title: Creation of artifacts with the test results from the service packages
+# Description: This script adds to a tar file the files located in the packages/<package>/tests dir of this repository. The netsim folders are ignored. The resulting tar file is stored in the mounted volume /tmp/nso for easier access via the pipeline later on.
+# Author: @ponchotitlan
+#
+# Usage:
+#   ./create-artifact-tests.sh <service-name>
+
+# This function creates a tar file of the folder specified and saves it in the /tmp/nso location
+# Usage tar_folders <container_name(str)> <package_folder_names(array(str))>
+run_robot_test(){
+    local container_name="$1"
+    local service_name="$2"
+
+    local TOKEN_SUCCESS="0 failed"
+    local output=$(docker exec -i $container_name bash -lc "cd /nso/run/packages/$service_name/tests && robot $service_name.robot")
+
+    if echo "$output" | grep -q "$TOKEN_SUCCESS"; then
+        # This test passed!
+        echo 1
+    else
+        # This test didn't pass!
+        echo 0
+    fi
+}
+
+YAML_FILE_CONFIG="pipeline/setup/config.yaml"
+YAML_FILE_DOCKER="pipeline/setup/docker-compose.yml"
+PACKAGES_DIR="packages"
+NEDS_PATH=".netsims | keys"
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <container_name> ..."
+    exit 1
+fi
+
+# Extract the name of the container and remove quotes
+CONTAINER_NAME_PATH=".services.$1.container_name"
+container_name=$(yq "$CONTAINER_NAME_PATH" "$YAML_FILE_DOCKER")
+container_name=$(echo "$container_name" | tr -d '"')
+
+# Extract the netsim folder names from the YAML file
+ned_packages=$(yq "$NEDS_PATH" "$YAML_FILE_CONFIG")
+
+# Get all the packages folders and remove the trailing slash from their names
+all_packages=($(ls -d "$PACKAGES_DIR"/*/ | xargs -n 1 basename))
+
+# Iterate over each folder and check if it's in the excluded list
+all_tests_passed=1
+for package in "${all_packages[@]}"; do
+
+    is_ned=0
+    for ned in $ned_packages; do
+        ned=$(echo "$ned" | tr -d '"')
+        ned=$(echo "$ned" | tr -d ',')
+        if [[ $package == $ned ]]; then
+            is_ned=1
+        fi
+    done
+
+    if [[ $is_ned == 0 ]]; then
+        this_test_pass=$(run_robot_test $container_name $package)
+
+        # If at least one test didn't pass. This job is declared a failure
+        if [[ $this_test_pass == 0 ]]; then
+            all_tests_passed=0
+        fi
+    fi
+done
+
+if [[ $all_tests_passed == 0 ]]; then
+    # The job failed
+    echo "failed"
+else
+    # The job is successful
+    echo "pass"
+fi
